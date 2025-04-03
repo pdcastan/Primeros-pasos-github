5d284ab1f9e583c15fb6809ca10aa19b1dcb9905
pdcastan <pcastanedam@unal.edu.co>
2025-04-03 09:23:24 -0500

    Create primerospasos.py

---
+++ primerospasos.py
@@ -0,0 +1,40 @@
+#Ejercicio1
+print("“Hola, ya se imprimir frases”")
+#Ejercicio2
+print(2706)
+#Ejercicio3
+print(25.2)
+#Ejercicio4
+a = 1234 + 532
+print(a)
+#Ejercicio5
+b = 1234 - 532
+print(b)
+#Ejercicio6
+c = 1234 * 532
+print(c)
+#Ejercicio7
+d = 1234 / 532
+print(d)
+#Ejercicio8
+i = 1
+while i <= 3:
+    print(i)
+    i += 1
+#Ejercicio9
+for i in range(1, 10):
+    print(i)
+#Ejercicio10
+i = 1
+while i <= 10000:
+    print(i)
+    i += 1
+#Ejercicio11
+for i in range(5, 11):
+    print(i)
+#Ejercicio12
+i = 5
+while i <= 15:
+    print(i)
+    i += 1
+#Ejercicio13
